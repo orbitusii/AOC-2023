99e170b60d0c7a69d16880b369fd379e113e0e0d
Dani Lodholm <dlodholm@icloud.com>
2023-12-01 19:17:48 -0700

    Add project files.

---
+++ Application/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day1;
+
+Console.WriteLine("Welcome to Advent of Code 2023!");
+Console.WriteLine("This is Robin's Unified Answer Application.");
+Console.WriteLine("Please peruse the following options:");
+
+while (ReadInput(Console.ReadLine()) == false) { Console.Write("Enter a Day (1-25):"); }
+
+static bool ReadInput (string? input)
+{
+    if (input is null || string.IsNullOrWhiteSpace(input)) return false;
+
+    string[] lines = File.ReadAllLines($"./Day{input}.txt");
+
+    if (input == "1")
+    {
+        Console.WriteLine(new Day1Solver().Solve(lines));
+    }
+
+    return true;
+}
---
+++ Day1/Day1Solver.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day1;
+
+public class Day1Solver
+{
+    public Day1Solver() { }
+
+    static readonly Regex Regex = new(@"(?<=(\d|one|two|three|four|five|six|seven|eight|nine|zero))+");
+
+    public int Solve(string[] lines)
+    {
+        Console.WriteLine(lines.Length + " lines to parse...");
+
+        int Sum = 0;
+
+        foreach (string line in lines)
+        {
+            Console.Write(line);
+            var matches = Regex.Matches(line);
+            var mf = matches.First();
+            var ml = matches.Last();
+            Console.Write($" has {matches.Count()} matches; we care about");
+            string first = mf.Groups[1].Value;
+            Console.Write($" {first}");
+            string last = ml.Groups[1].Value;
+            Console.Write($" and { last}");
+
+            int mashedTogether = ParseInt(first) * 10  + ParseInt(last);
+
+            Console.Write($", yielding {mashedTogether}\n");
+            Sum += mashedTogether;
+        }
+
+        return Sum;
+    }
+
+    public int ParseInt(string alpha) =>
+        alpha.ToLower() switch
+        {
+            "one" or "1" => 1,
+            "two" or "2" => 2,
+            "three" or "3" => 3,
+            "four" or "4" => 4,
+            "five" or "5" => 5,
+            "six" or "6" => 6,
+            "seven" or "7" => 7,
+            "eight" or "8" => 8,
+            "nine" or "9" => 9,
+            "zero" or "0"=> 0,
+            _ => throw new InvalidCastException(),
+        };
+}
